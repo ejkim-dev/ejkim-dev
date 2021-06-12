9e3367abc7239b7261ffbb6048aeb064ce120c2c
ejkim-dev <57036748+ejkim-dev@users.noreply.github.com>
2021-06-12 22:44:54 +0900

    [create] 코테 스터디

---
+++ TIL/CodingTest/Kotlin/src/main/kotlin/algorithm/sort/sortNumber2.kt
@@ -0,0 +1,8 @@
+package algorithm.sort
+
+import java.util.*
+
+fun SortNumber2():Unit {
+    val size : Int = readLine()!!.toInt()
+    val arr = Array<Int>(size) { readLine()!!.toInt()}
+}
---
+++ TIL/CodingTest/Kotlin/src/main/kotlin/algorithm/sort/sortNumber3.kt
@@ -0,0 +1,4 @@
+package algorithm.sort
+
+class sortNumber3 {
+}
